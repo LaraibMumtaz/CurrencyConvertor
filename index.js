9e4745da42463c4c824de7611646c73385f95866
Laraib Mumtaz <laraibmumtaz07@gmail.com>
2024-06-22 01:11:29 +0500

    CurrencyConvert

---
+++ index.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+const rates = {
+    USD: 1, //BAse currency
+    EUR: 0.91,
+    PKR: 280,
+    DINAR: 0.38,
+    YUAN: 7.26
+};
+let answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter from Currency: ",
+        type: "list",
+        choices: ['USD', 'EUR', 'PKR', 'DINAR', 'YUAN']
+    },
+    {
+        name: "To",
+        message: "Enter To Currency: ",
+        type: "list",
+        choices: ['USD', 'EUR', 'PKR', 'DINAR', 'YUAN']
+    },
+    {
+        name: "amount",
+        message: "enter your amount",
+        type: "number"
+    }
+]);
+let fromCurrency = rates[answer.from];
+let toAmount = rates[answer.To];
+let amount = answer.amount;
+let baseAmount = amount / fromCurrency;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
